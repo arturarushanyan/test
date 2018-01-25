9934171c0ad2fb0b7277116aa636502d4fd2f2d7
SOFTCONSTRUCT\tigran.sargsyan <tigran.sargsyan@riotagaming.com>
2018-01-25 15:20:57 +0400

    new version

--- models/userModel.js
+++ models/userModel.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose');
 
-//mongoose.connect('mongodb://localhost/riotatest');
+mongoose.connect('mongodb://localhost/riotatest');
 // mongoose.connect('mongodb://riota:Riot$123@ds251737.mlab.com:51737/riotadb');
+/*
 mongoose.connect('mongodb://127.0.0.1:27017/riota', (err,db) => {
     if (err){
         console.log('something went wrong', err);
@@ -9,6 +10,7 @@ mongoose.connect('mongodb://127.0.0.1:27017/riota', (err,db) => {
         console.log("connected to riota database", db);
     }
 });
+*/
 
 let db = mongoose.connection;
 
