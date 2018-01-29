0ed2f874d20fe2834e07378539a04c0ebbb27d37
SOFTCONSTRUCT\artur.arushanyan <arthur.arushanyan92@gmail.com>
2018-01-29 18:30:53 +0400

    db change

--- models/userModel.js
+++ models/userModel.js
@@ -1,15 +1,15 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost/riotatest');
+//mongoose.connect('mongodb://localhost/riotatest');
 // mongoose.connect('mongodb://riota:Riot$123@ds251737.mlab.com:51737/riotadb');
 
-/*mongoose.connect('mongodb://127.0.0.1:27017/riota', (err,db) => {
+mongoose.connect('mongodb://127.0.0.1:27017/riota', (err,db) => {
     if (err){
         console.log('something went wrong', err);
     } else {
         console.log("connected to riota database", db);
     }
-});*/
+});
 
 
 let db = mongoose.connection;
